83b2213269e48a7c2078c52412785afe682dc422
zahid2873 <mahdi.zhm13@gmail.com>
2023-04-14 16:42:17 +0600

    api integration commit

--- lib/screen/login_page.dart
+++ lib/screen/login_page.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
 import 'package:admin_app_ecommerce/screen/navbar/home_page.dart';
 import 'package:admin_app_ecommerce/widget/custome_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -13,83 +17,117 @@ class _LoginPageState extends State<LoginPage> {
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
+  String baseUrl = "http://temp.techsolutions-bd.com/api/admin/";
 
   bool isObscure=true;
   int pressCount=0;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        padding: EdgeInsets.all(20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text("Login",style: textStyle(28,Colors.blue,FontWeight.bold),),
-            Form(key:_formKey, child: Column(
-              children: [
-                SizedBox(height: 20,),
-                TextFormField(
-                  keyboardType: TextInputType.emailAddress,
-                  controller: emailController,
-                  decoration: InputDecoration(
-                    hintText: "Enter your email",
-                    border: OutlineInputBorder(),
-                  ),
-                  validator:(value){
-                    if(value == null || value.isEmpty || !value.contains('@') || !value.contains('.')){
-                      return 'Invalid Email';
-                    }
-                    return null;
-                  },
-                ),
-                SizedBox(height: 20,),
-                TextFormField(
-                  controller: passwordController,
-                  keyboardType: TextInputType.visiblePassword,
-                  obscureText: isObscure,
-                  decoration: InputDecoration(
-                    hintText: "Enter your password",
-                    suffixIcon:IconButton(onPressed: (){
-                      setState(() {
-                        if(pressCount % 2==0){
-                          isObscure=false;
-                        }else{
-                          isObscure=true;
-                        }
-                        pressCount++;
-                      }
-                      );
-                    }, icon: pressCount%2==0? Icon(Icons.visibility): Icon(Icons.visibility_off)
+    return ModalProgressHUD(
+      inAsyncCall: isLoading,
+      opacity: .2,
+      blur: .5,
+      progressIndicator: spinkit,
+      child: Scaffold(
+        body: Container(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text("Login",style: textStyle(28,Colors.blue,FontWeight.bold),),
+              Form(key:_formKey, child: Column(
+                children: [
+                  SizedBox(height: 20,),
+                  TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    controller: emailController,
+                    decoration: InputDecoration(
+                      hintText: "Enter your email",
+                      border: OutlineInputBorder(),
                     ),
-                    border: OutlineInputBorder(),
+                    validator:(value){
+                      if(value == null || value.isEmpty || !value.contains('@') || !value.contains('.')){
+                        return 'Invalid Email';
+                      }
+                      return null;
+                    },
                   ),
-                  validator:(value){
-                    if(value!.isEmpty) {
-                      return "Please enter password";
-                    }
-                    return null;
-                  },
-                ),
-                SizedBox(height: 20,),
-                MaterialButton(
-                  height: 60,
-                  minWidth: double.infinity,
-                  color: Colors.blue,
-                  onPressed: (){
-                    if(_formKey.currentState!.validate()){
-                      Navigator.push(context, MaterialPageRoute(builder: (context)=>HomePage()));
-                    }
+                  SizedBox(height: 20,),
+                  TextFormField(
+                    controller: passwordController,
+                    keyboardType: TextInputType.visiblePassword,
+                    obscureText: isObscure,
+                    decoration: InputDecoration(
+                      hintText: "Enter your password",
+                      suffixIcon:IconButton(onPressed: (){
+                        setState(() {
+                          if(pressCount % 2==0){
+                            isObscure=false;
+                          }else{
+                            isObscure=true;
+                          }
+                          pressCount++;
+                        }
+                        );
+                      }, icon: pressCount%2==0? Icon(Icons.visibility): Icon(Icons.visibility_off)
+                      ),
+                      border: OutlineInputBorder(),
+                    ),
+                    validator:(value){
+                      if(value!.isEmpty) {
+                        return "Please enter password";
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 20,),
+                  MaterialButton(
+                    height: 60,
+                    minWidth: double.infinity,
+                    color: Colors.blue,
+                    onPressed: (){
+                      if(_formKey.currentState!.validate()){
+                        getLogin();
+                        //Navigator.push(context, MaterialPageRoute(builder: (context)=>HomePage()));
+                      }
 
-                  },
-                  child: Text('Login'),
-                )
-              ],
-            ))
-          ],
+                    },
+                    child: Text('Login'),
+                  )
+                ],
+              ))
+            ],
+          ),
         ),
       ),
     );
+  }
+  bool isLoading=false;
+  getLogin()async{
+    try{
+      setState(() {
+        isLoading=true;
+      });
+      String url = "${baseUrl}sign-in";
+      var map = Map<String, dynamic>();
+      map["email"] = emailController.text.toString();
+      map["password"] = passwordController.text.toString();
+      var responce = await http.post(Uri.parse(url),body: map);
+      var data = jsonDecode(responce.body);
+      setState(() {
+        isLoading=false;
+        if(responce.statusCode==200){
+          Navigator.push(context, MaterialPageRoute(builder: (context)=>HomePage()));
+        }else{
+          print("Api integration is unsuccessful");
+        }
+      });
+      print("Responce is ${data}");
+      print("Status code is ${responce.statusCode}");
+    }catch(e){
+      print("Something wrong $e");
+    }
   }
 
 }
--- lib/widget/custome_widget.dart
+++ lib/widget/custome_widget.dart
@@ -1,6 +1,8 @@
 
 import 'dart:ui';
 
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 textStyle(double size,[ Color ? clr, FontWeight ? fw ]){
@@ -10,3 +12,13 @@ textStyle(double size,[ Color ? clr, FontWeight ? fw ]){
     color: clr,
   );
 }
+
+final spinkit = SpinKitFadingFour(
+  itemBuilder: (BuildContext context, int index) {
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        color: index.isEven ? Colors.red : Colors.green,
+      ),
+    );
+  },
+);
