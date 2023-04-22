85e6f69c01437a726a8e758f7cd740783f9d92d9
zahid2873 <mahdi.zhm13@gmail.com>
2023-04-22 23:29:31 +0600

    add category image upload

--- lib/screen/add_category.dart
+++ lib/screen/add_category.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class AddCategory extends StatefulWidget {
   const AddCategory({Key? key}) : super(key: key);
@@ -9,6 +12,26 @@ class AddCategory extends StatefulWidget {
 
 class _AddCategoryState extends State<AddCategory> {
   TextEditingController nameController = TextEditingController();
+  File ? icon, image;
+  ImagePicker picker = ImagePicker()
+
+  getIconFromGallery()async{
+    final pickImage = await picker.pickImage(source: ImageSource.gallery);
+    if(pickImage!=null){
+      setState(() {
+        icon=File(pickImage.path);
+      });
+    }
+  }
+  getImageFromGallery()async{
+    final pickImage = await picker.pickImage(source: ImageSource.gallery);
+    if(pickImage!=null){
+      setState(() {
+        image=File(pickImage.path);
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
@@ -29,16 +52,70 @@ class _AddCategoryState extends State<AddCategory> {
 
             Text("Add Icon"),
             Container(
-              color: Colors.red,
+              color: icon==null?Colors.grey : null,
               height: size. height * .2,
               width: size.width * .5,
+              child:icon == null? InkWell(
+                onTap: (){
+
+                },
+                child: Column(
+                children: [
+                  Icon(Icons.photo),
+                  Text("Upload Icon")
+                ],
+            ),
+              ): Stack(
+                children: [
+                  Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: FileImage(icon!),
+                    )
+                  ),
+            ),
+                  Positioned(child: IconButton(onPressed: (){
+                    setState(() {
+                      icon = null;
+                    });
+                  }, icon: Icon(Icons.cancel_outlined)))
+                ],
+              )
             ),
             SizedBox(height: 20,),
             Text("Add Category item"),
             Container(
-              color: Colors.teal,
+              color: image==null?Colors.grey:null,
               height: size.height * 2,
               width: size.width * .9,
+                child:image == null? InkWell(
+                  onTap: (){
+                    getImageFromGallery;
+                  },
+                  child: Column(
+                    children: [
+                      Icon(Icons.photo),
+                      Text("Upload Image")
+                    ],
+                  ),
+                ): Stack(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: FileImage(image!),fit: BoxFit.cover,
+                          )
+                      ),
+                    ),
+                    Positioned(child: IconButton(onPressed: (){
+                      setState(() {
+                        image=null;
+                      });
+                    },
+                      icon: Icon(Icons.cancel_outlined
+                    ),))
+                  ],
+                )
             )
           ],
         ),
